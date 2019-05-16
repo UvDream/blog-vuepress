ce12b80907d06204aae39e244466a6463d30819a
wangzhongjie <uvdream@163.com>
2019-05-16 19:48:24 +0800

    设置目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,39 @@
 module.exports = {
-    title: '汪中杰',
-    description: '一切皆有可能!!',
-    base: "/blog-vuepress/",
-    
+  base:'blog',
+  title: '汪中杰',
+  description: '一切皆有可能!',
+  base: "/blog-vuepress/",
+  themeConfig: {
+    //   导航栏
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '前端',
+        link: '/web/'
+      },
+      {
+        text: 'Go',
+        link: '/go/'
+      },
+      {
+        text: 'Flutter',
+        link: '/flutter/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/UvDream'
+      },
+    ],
+    sidebar:{
+      '/web/':[
+        "",
+        "web1"
+      ]
+    },
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+  }
 }
