a7beff8b5c6952826de780675dd885df7c8261ca
wangzhongjie <uvdream@163.com>
2019-05-29 10:50:15 +0800

    主题

---
+++ lib/components/InfoCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    个人信息
+  </div>
+</template>
--- lib/layouts/GlobalLayout.vue
+++ lib/layouts/GlobalLayout.vue
@@ -1,13 +1,16 @@
 <template>
     <div>   
         <Header />
+        <Main />
     </div>
 </template>
 <script>
 import Header from "../components/Header"
+import Main from "../components/Main"
 export default {
     components:{
-        Header
+        Header,
+        Main
     }
 }
 </script>
