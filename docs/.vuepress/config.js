58e28bb655782f382418a348f070634d582edc89
wangzhongjie <uvdream@163.com>
2019-07-22 13:49:00 +0800

    [feat]flutter 插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,179 +1,184 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    base: 'blog',
-    title: 'U世界的V梦想',
-    description: '一切皆有可能!',
-    base: "/blog-vuepress/",
-    port: "2222",
-    // theme: path.resolve(__dirname, '../../lib'),
-    
-    themeConfig: {
-        //   导航栏
-        nav: [{
-                text: '首页',
-                link: '/'
-            },
-            {
-                text: '前端',
-                items: [{
-                        text: "Web",
-                        link: '/web/'
-                    },
-                    {
-                        text: 'JavaScript',
-                        link: '/javascript/'
-                    },
-                    {
-                        text: 'Angular',
-                        link: '/angular/'
-                    },
-                    {
-                        text: 'Vue',
-                        link: '/vue/'
-                    },
-                    {
-                        text: 'React',
-                        link: '/react/'
-                    },
-                    {
-                        text: '面试',
-                        link: '/interview/'
-                    }
-                ]
-            },
-            {
-                text: 'Go',
-                link: '/go/'
-            },
-            {
-                text: 'Flutter',
-                items:[
-                    {
-                        text:'基础',
-                        link:'/flutter/'
-                    },
-                    {
-                        text:'深入',
-                        link:'/flutter-deep/'
-                    }
-                ]
-            },
-            {
-                text:'规范',
-                link:'/specification/'
-            },
-            {
-                text: 'Github',
-                link: 'https://github.com/UvDream'
-            },
-        ],
-        sidebar: {
-            '/web/': [{
-                title: 'Web',
-                collapsable: false,
-                children: [
-                    "",
-                    "css"
-                ]
-            }],
-            '/javascript/': [{
-                title: "JavaScript",
-                collapsable: false,
-                children: [
-                    "",
-                    'js1',
-                    'closure',
-                    'design',
-                    'canvas',
-                    'canvas_api',
-                ]
-            }],
-            '/vue/': [{
-                title: "Vue",
-                collapsable: false,
-                children: [
-                    "",
-                    "vue1",
-                    "vue2"
-                ]
-            }],
-            '/react/': [{
-                title: "React",
-                collapsable: false,
-                children: [
-                    "",
-                    "props",
-                    "vscode",
-                    "life",
-                    "other"
-                ]
-            }],
-            '/specification/':[
-                {
-                title:"代码规范",
-                collapsable: false,
-                children:[
-                    "",
-                    "git"
-                ]
-                }
-            ],
-            '/interview/': [{
-                title: "面试",
-                collapsable: false,
-                children: [
-                    "",
-                    "css",
-                    "javascript",
-                    "vue",
-                    "typescript",
-                    "react",
-                    "code",
-                    "type",
-                    "http",
-                    "catch"
-                ]
-            }],
-            '/flutter/': [{
-                title: 'Flutter',
-                collapsable: false,
-                children: [
-                    "",
-                    "flutter1",
-                    "vscode",
-                    "dio",
-                    "plugin",
-                    "iconfont",
-                    "state",
-                    "model"
-                ]
-            }]
-
-        },
-        sidebarDepth: 2,
-        lastUpdated: '最后更新时间'
-    },
-    plugins: [
-        '@vuepress/back-to-top',
-        '@vuepress/nprogress',
-        ['@vuepress/pwa',{
-            serviceWorker: true,
-            updatePopup: true,
-            updatePopup: {
-                message: "有新内容更新!",
-                buttonText: "刷新"
-            }
-        }],
-        ['@vssue/vuepress-plugin-vssue', {
-            platform: 'github',
-            owner: 'UvDream',
-            repo: 'blog-vuepress',
-            clientId: '1ea5b28de21b18dec3ee',
-            clientSecret: '44ab2b53a57aacf09f894f50da5e954ff02dd949',
-        }],
+  base: "blog",
+  title: "U世界的V梦想",
+  description: "一切皆有可能!",
+  base: "/blog-vuepress/",
+  port: "2222",
+  // theme: path.resolve(__dirname, '../../lib'),
+  themeConfig: {
+    //   导航栏
+    nav: [
+      {
+        text: "首页",
+        link: "/"
+      },
+      {
+        text: "前端",
+        items: [
+          {
+            text: "Web",
+            link: "/web/"
+          },
+          {
+            text: "JavaScript",
+            link: "/javascript/"
+          },
+          {
+            text: "Angular",
+            link: "/angular/"
+          },
+          {
+            text: "Vue",
+            link: "/vue/"
+          },
+          {
+            text: "React",
+            link: "/react/"
+          },
+          {
+            text: "面试",
+            link: "/interview/"
+          }
+        ]
+      },
+      {
+        text: "Go",
+        link: "/go/"
+      },
+      {
+        text: "Flutter",
+        items: [
+          {
+            text: "基础",
+            link: "/flutter/"
+          },
+          {
+            text: "插件",
+            link: "/flutter-plugin/"
+          },
+          {
+            text: "深入",
+            link: "/flutter-deep/"
+          }
+        ]
+      },
+      {
+        text: "规范",
+        link: "/specification/"
+      },
+      {
+        text: "Github",
+        link: "https://github.com/UvDream"
+      }
     ],
-	 markdown: {
+    sidebar: {
+      "/web/": [
+        {
+          title: "Web",
+          collapsable: false,
+          children: ["", "css"]
+        }
+      ],
+      "/javascript/": [
+        {
+          title: "JavaScript",
+          collapsable: false,
+          children: ["", "js1", "closure", "design", "canvas", "canvas_api"]
+        }
+      ],
+      "/vue/": [
+        {
+          title: "Vue",
+          collapsable: false,
+          children: ["", "vue1", "vue2"]
+        }
+      ],
+      "/react/": [
+        {
+          title: "React",
+          collapsable: false,
+          children: ["", "props", "vscode", "life", "other"]
+        }
+      ],
+      "/specification/": [
+        {
+          title: "代码规范",
+          collapsable: false,
+          children: ["", "git"]
+        }
+      ],
+      "/interview/": [
+        {
+          title: "面试",
+          collapsable: false,
+          children: [
+            "",
+            "css",
+            "javascript",
+            "vue",
+            "typescript",
+            "react",
+            "code",
+            "type",
+            "http",
+            "catch"
+          ]
+        }
+      ],
+      "/flutter/": [
+        {
+          title: "Flutter",
+          collapsable: false,
+          children: [
+            "",
+            "flutter1",
+            "vscode",
+            "dio",
+            "iconfont",
+            "state",
+            "model"
+          ]
+        }
+      ],
+      "/flutter-plugin/": [
+        {
+          title: "Flutter插件",
+          collapsable: false,
+          children: [""]
+        }
+      ]
+    },
+    sidebarDepth: 2,
+    lastUpdated: "最后更新时间"
+  },
+  plugins: [
+    "@vuepress/back-to-top",
+    "@vuepress/nprogress",
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true,
+        updatePopup: {
+          message: "有新内容更新!",
+          buttonText: "刷新"
+        }
+      }
+    ],
+    [
+      "@vssue/vuepress-plugin-vssue",
+      {
+        platform: "github",
+        owner: "UvDream",
+        repo: "blog-vuepress",
+        clientId: "1ea5b28de21b18dec3ee",
+        clientSecret: "44ab2b53a57aacf09f894f50da5e954ff02dd949"
+      }
+    ]
+  ],
+  markdown: {
     lineNumbers: true
   }
-}
+};
